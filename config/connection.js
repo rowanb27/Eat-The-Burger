e8bc300e13cdbc7835fc71ec472f90d3b16b3cb8
Robert Rowan <rowanb27@yahoo.com>
2019-07-23 17:36:41 -0500

    added connection.js

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "9102QycuL",
+        database: "burgers_db"
+    });
+};
