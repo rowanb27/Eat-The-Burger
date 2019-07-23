a1a3350313b61cc473445978d8588da0ad0ec5a0
Robert Rowan <rowanb27@yahoo.com>
2019-07-23 17:41:43 -0500

    Created printQuestionMarks and ObjToSql function

---
+++ config/orm.js
@@ -0,0 +1,30 @@
+var connection = require("../config/connection.js");
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }   
+        return arr.toString();
+};
+function objToSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'"; 
+            }   arr.push(key + "=" + value);
+        }  
+    }   
+                return arr.toString();
+};
+var orm = {
+    selectAll: function(tableInput, callback) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }   
+                callback(result);
+        });
+    },
