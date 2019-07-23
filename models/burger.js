a8066552922b0bdb34e00d7ff6951767f3e71456
Robert Rowan <rowanb27@yahoo.com>
2019-07-23 17:48:40 -0500

    Callbacks added

---
+++ models/burger.js
@@ -0,0 +1,31 @@
+var orm = require("../config/orm.js");
+var burger = {
+    selectAll: function(callback) {
+        orm.selectAll("burgers", 
+            function(res) {
+                callback(res);
+        });
+    },
+    insertOne: function(cols, vals, callback) {
+        orm.insertOne("burgers",
+            cols, vals, 
+            function(res) {
+                callback(res);
+        });
+    },
+    updateOne: function(objColVals, condition, callback) {
+        orm.updateOne("burgers", 
+            objColVals, condition, 
+            function(res) {
+                callback(res);
+        });
+    },
+    deleteOne: function(condition, callback) {
+        orm.deleteOne("burgers", 
+            condition, 
+            function(res) {
+                callback(res);
+        });
+    }
+};
+module.exports = burger;
